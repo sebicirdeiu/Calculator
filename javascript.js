fd078e90c22e09322806cbec35cd9e59c96c0539
Sebi Cirdeiu <sebbcirdeiu@yahoo.com>
2023-01-08 17:57:25 +0200

    Create working calculator for simple math operations

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+
 sumAll = (a,b) => {
     let total = a + b;
     return total;
@@ -45,4 +46,105 @@ function operate (a, operator, b) {
 
 };
 
-console.log(operate( 5, "**" , 5))
+
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const zero = document.querySelector(".zero");
+const dot = document.getElementById("dot");
+const division = document.getElementById("devide");
+const multiplication = document.getElementById("multiply");
+const minus = document.getElementById("minus");
+const plus = document.getElementById("plus");
+const ac = document.getElementById("ac");
+const pow = document.getElementById("pow");
+const sqr = document.getElementById("sqr");
+const equal = document.getElementById("equal");
+
+
+const display = document.querySelector(".display");
+display.textContent = ""
+
+
+one.addEventListener("click", function() {
+    display.textContent += "1"}); 
+
+two.addEventListener("click", function()
+   {display.textContent += "2"});
+
+three.addEventListener("click", function()
+   {display.textContent += "3"});
+
+four.addEventListener("click", function()
+   {display.textContent += "4"});
+
+five.addEventListener("click", function()
+   {display.textContent += "5"});
+
+six.addEventListener("click", function()
+   {display.textContent += "6"});
+
+seven.addEventListener("click", function()
+   {display.textContent += "7"});
+
+eight.addEventListener("click", function()
+   {display.textContent += "8"});
+
+nine.addEventListener("click", function()
+   {display.textContent += "9"});
+
+zero.addEventListener("click", () => {display.textContent += "0"});
+
+dot.addEventListener("click", () => {display.textContent += "."});
+
+division.addEventListener("click", () => {
+    display.textContent += "/"});
+
+multiplication.addEventListener("click", () => {
+        display.textContent += "*"});
+
+minus.addEventListener("click", () => {
+            display.textContent += "-"});
+
+plus.addEventListener("click", () =>
+{display.textContent += "+"} );
+
+ac.addEventListener("click", () => {
+display.textContent = "" } );
+
+equal.addEventListener("click", () => {
+
+    const displayText = display.textContent;
+    const plusIndex = displayText.indexOf('+');
+    const minusIndex = displayText.indexOf('-');
+    const multiplyIndex = displayText.indexOf('*');
+    const divideIndex = displayText.indexOf('/');
+    let operatorIndex;
+    let operator;
+    if (plusIndex !== -1) {
+      operatorIndex = plusIndex;
+      operator = '+';
+    } else if (minusIndex !== -1) {
+      operatorIndex = minusIndex;
+      operator = '-';
+    } else if (multiplyIndex !== -1) {
+      operatorIndex = multiplyIndex;
+      operator = '*';
+    } else if (divideIndex !== -1) {
+      operatorIndex = divideIndex;
+      operator = '/';
+    }
+    const firstNum = parseFloat(displayText.substring(0, operatorIndex));
+    const secondNum = parseFloat(displayText.substring(operatorIndex + 1));
+  
+    const result = parseFloat(operate(firstNum, operator, secondNum).toFixed(2));
+  
+    
+    display.textContent = result;
+  });
